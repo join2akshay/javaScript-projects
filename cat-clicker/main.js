a9e600887161507c7c1052c0f411dd580df11213
tvibhu12 <tvibhu12@gmail.com>
2019-02-25 23:45:06 +0530

    update file

--- cat-clicker/main.js
+++ cat-clicker/main.js
@@ -1,21 +1,34 @@
 const fig1=document.getElementById('cat1');
+const fig2=document.getElementById('cat2');
 const div1=document.getElementById('name');
 
 
-show_image('https://farm3.staticflickr.com/2298/2290467335_89067c7b51_b.jpg',200,200,'cat')
-//show_image('https://farm2.staticflickr.com/1126/625069434_db86b67df8_b.jpg',200,200,'cat2')
+show_image('https://farm3.staticflickr.com/2298/2290467335_89067c7b51_b.jpg',200,200,'cat');
+show_image2('https://farm2.staticflickr.com/1126/625069434_db86b67df8_b.jpg',200,200,'cat2')
 figc();
-show_name('Jemmy','Nemmy');
-function show_image(src, width, height, alt) {
+//show_name('Jemmy','Nemmy');
+function show_image(src, width, height, alt,fig) {
     var img = document.createElement("img");
     img.src = src;
     img.width = width;
     img.height = height;
     img.alt = alt;
 
-    // This next line will just add it to the <DIV> tag
-    
+    // This next line will just add it to the <figure> tag
     fig1.appendChild(img);
+  
+    
+}
+// Display image 2
+function show_image2(src, width, height, alt,fig) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
+
+    // This next line will just add it to the <figure> tag
+    fig2.appendChild(img);
   
     
 }
@@ -23,7 +36,7 @@ function show_image(src, width, height, alt) {
 function figc(){
     const figc=document.createElement("figcaption");
     figc.innerText='Jemmy';
-    //const span2=document.createElement("span");
+    const span2=document.createElement("span");
     fig1.appendChild(figc);
    
     
