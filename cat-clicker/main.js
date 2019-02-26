09db278223953fa4b0f3695f24c880ee2914088e
tvibhu12 <tvibhu12@gmail.com>
2019-02-26 22:48:10 +0530

    add count function

--- cat-clicker/main.js
+++ cat-clicker/main.js
@@ -1,12 +1,15 @@
 const fig1=document.getElementById('cat1');
 const fig2=document.getElementById('cat2');
 const div1=document.getElementById('name');
+let count=0;
+let count2=0;
 
 
 show_image('https://farm3.staticflickr.com/2298/2290467335_89067c7b51_b.jpg',200,200,'cat');
 show_image2('https://farm2.staticflickr.com/1126/625069434_db86b67df8_b.jpg',200,200,'cat2')
 figc();
-//show_name('Jemmy','Nemmy');
+figc2();
+//sDisplay image 1
 function show_image(src, width, height, alt,fig) {
     var img = document.createElement("img");
     img.src = src;
@@ -32,21 +35,38 @@ function show_image2(src, width, height, alt,fig) {
   
     
 }
-
+// figure caption
 function figc(){
     const figc=document.createElement("figcaption");
     figc.innerText='Jemmy';
-    const span2=document.createElement("span");
     fig1.appendChild(figc);
    
     
    
-//div1.appendChild(span);
-//div1.appendChild(span2);
+
 
 };
-//function show_name(name,name1){
- //document.getElementById('name').innerText=name+" "+name1;
- //document.getElementById('name2').innerText=name1;
-    //document.getElementById('name').innerText=name2;
-//};
+//figure caption for second photo
+function figc2(){
+    const figc=document.createElement("figcaption");
+    figc.innerText='Nemmy';
+    fig2.appendChild(figc);
+};
+
+var elem = document.getElementById('cat1');
+elem.addEventListener('click', function(){
+ //the element has been clicked... 
+    count++;
+ const p1=document.getElementById("click1");
+p1.innerText='total click on my photo ='+count;
+  
+}, false);
+
+var elem = document.getElementById('cat2');
+elem.addEventListener('click', function(){
+ //the element has been clicked... 
+    count2++;
+ const p1=document.getElementById("click2");
+p1.innerText='total click on my photo='+count2;
+  
+}, false);
