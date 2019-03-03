cc71509ddf79f5179952a5f2d9feb6d5df0e29d5
tvibhu12 <tvibhu12@gmail.com>
2019-03-03 23:17:47 +0530

    update file

--- cat-clicker-premium/main.js
+++ cat-clicker-premium/main.js
@@ -10,19 +10,45 @@ const name=['Jenny','Nenny','Shely','Kenny','aenny'];
   });
 
   */
- for(i=0;i<=4;i++)
+ /*
+  for(i=0;i<=4;i++)
  {
      const img = new Image();
      img.src = data[i];
     img.setAttribute("class", "banner-img");
     img.setAttribute("alt", "effy");
-    document.getElementById("img-container").appendChild(img);
+    document.getElementById("cat1").appendChild(img);
 
-     console.log(data[i]);
+    // console.log(data[i]);
  };
+ */
+/*
  for(i=0;i<=4;i++)
  {
-   var j= document.getElementById('name').innerHTML=`<p>${name[i]}</p>`;
+  const figc=document.createElement("figcaption");
+  figc.innerText=name[i];
+  document.getElementById("cat1").appendChild(figc);
+  // var j= document.getElementById('name').innerHTML=`<p>${name[i]}</p>`;
    
      //  console.log(data[i]);
  };
+
+ */
+
+for(i=0;i<=4;i++)
+{
+  var x = document.createElement("LI");
+  var t = document.createTextNode(name[i]);
+  x.appendChild(t);
+ 
+
+x.addEventListener('click',(function(num){
+  return function() {
+    
+};  
+})(name[i]));
+document.getElementById('name').appendChild(x);
+}
+
+
+
